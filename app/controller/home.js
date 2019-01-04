a94fcb8d5f8a2d8d959cee4f050ae10ccf9d1f85
wuyt <wuyt@cyclecentury.com>
2019-01-04 11:27:09 +0800

    Add react(client renderer).

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,8 +1,14 @@
 const Controller = require('egg').Controller;
+const fs = require('fs');
 
 class HomeController extends Controller {
     async index() {
         this.ctx.body = 'Hello world';
+    }
+
+    async render() {
+        //await this.ctx.render('html/index.html');
+        this.ctx.body = fs.readFileSync('client/dist/index.html', 'utf8');
     }
 }
 
--- config/config.default.js
+++ config/config.default.js
@@ -1,1 +1,8 @@
-exports.keys = 'c1o2o3k4i5e';
+const path = require('path');
+
+module.exports = (appInfo) => ({
+    static: {
+        dir: path.join(appInfo.baseDir, 'client/public'),
+    },
+    keys: 'c1o2o3k4i5e',
+});

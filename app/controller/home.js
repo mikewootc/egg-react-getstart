8bc60fba9cbcc70315df8419007a5958dc2b009c
wuyt <wuyt@cyclecentury.com>
2019-01-22 17:36:35 +0800

    Add mongoose and user management.

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,12 +3,18 @@ const fs = require('fs');
 
 class HomeController extends Controller {
     async index() {
+        console.log('HomeController index');
         this.ctx.body = 'Hello world';
     }
 
     async render() {
         //await this.ctx.render('html/index.html');
-        this.ctx.body = fs.readFileSync('client/dist/index.html', 'utf8');
+        this.ctx.body = fs.readFileSync('client/public/index.html', 'utf8');
+    }
+
+    async testToken() {
+        console.log('testToken. user:', this.ctx.state.user._doc);
+        this.ctx.body = {result: 'ok', username: this.ctx.state.user._doc.username};
     }
 }
 
